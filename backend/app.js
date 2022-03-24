ea0d87921929f15e6878751a196bff7b066d2c92
jda <https://github.com/jda447/Piiquante.git>
2022-03-24 01:36:38 +0000

    app & server.js/express & mongoose

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+//mongodb+srv://jon:<password>@cluster0.9kwsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://jon:bluebirds1927@cluster0.9kwsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+
+app.use((req, res, next) => {
+  console.log('Request received!');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Your request was successful!' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Response sent successfully!');
+});
+
+module.exports = app;
