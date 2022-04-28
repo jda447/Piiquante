d9eeedc5077c8599ee63855d19724be5779a40a7
jda <https://github.com/jda447/Piiquante.git>
2022-04-28 18:52:20 +0100

    removed required

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -8,8 +8,8 @@ const sauceSchema = mongoose.Schema({
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number, required: true },
-    dislikes: { type: Number, required: true },
+    likes: { type: Number },
+    dislikes: { type: Number },
     usersLiked: [{type: String}],
     usersDisliked: [{type: String}]
 });
